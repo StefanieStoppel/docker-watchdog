35ef94447c7b2d57d4c5e6bb375dd7f7944fd966
Stefanie Stoppel <s.stoppel@gmx.de>
2020-07-02 11:06:11 +0200

    feat: add names when running containers

--- run_docker_watchdog_1.sh
+++ run_docker_watchdog_1.sh
@@ -4,4 +4,4 @@ DEFAULT_DIR="$HOME/Desktop/test"
 HOST_MOUNT_DIR=${1:-$DEFAULT_DIR}
 CONTAINER_MOUNT_DIR=/app/data
 
-docker run -e "INPUT_DIR=$CONTAINER_MOUNT_DIR" -v "$HOST_MOUNT_DIR":"$CONTAINER_MOUNT_DIR" -it docker-watchdog-1:latest
+docker run --name docker-watchdog-1 -e "INPUT_DIR=$CONTAINER_MOUNT_DIR" -v "$HOST_MOUNT_DIR":"$CONTAINER_MOUNT_DIR" -it docker-watchdog-1:latest
--- run_docker_watchdog_2.sh
+++ run_docker_watchdog_2.sh
@@ -4,4 +4,4 @@ DEFAULT_DIR="$HOME/Desktop/test"
 HOST_MOUNT_DIR=${1:-$DEFAULT_DIR}
 CONTAINER_MOUNT_DIR=/app/data
 
-docker run -e "INPUT_DIR=$CONTAINER_MOUNT_DIR" -v "$HOST_MOUNT_DIR":"$CONTAINER_MOUNT_DIR" -it docker-watchdog-2:latest
+docker run --name docker-watchdog-2 -e "INPUT_DIR=$CONTAINER_MOUNT_DIR" -v "$HOST_MOUNT_DIR":"$CONTAINER_MOUNT_DIR" -it docker-watchdog-2:latest
